f20633d43757cef44a7946f724d10e62f732843b
Daniela Baron <baron.daniela@gmail.com>
2018-08-29 18:38:19 -0400

    style invalid form elements

--- src/components/data-entry/data-entry.js
+++ src/components/data-entry/data-entry.js
@@ -28,18 +28,20 @@ class DataEntry extends Component {
   // need "dirty checking" - do not validate until user has interacted with element
   // alos need to disable SHOW ME button
   validate(grams) {
+    let result = {};
     const intGrams = parseInt(grams, 10);
-    let result = { valid: true, message: null };
     if (!Number.isInteger(intGrams) || intGrams < 1 || intGrams > 50) {
-      result.valid = false;
-      result.message = 'Please enter a number between 1 and 50';
+      result = {
+        grams: { hasError: true, message: 'Please enter a number between 1 and 50'}
+      }
     }
     return result;
   }
 
   render() {
     const errors = this.validate(this.state.grams);
-    console.log(errors);
+    const isDisabled = Object.keys(errors).some(x => errors[x]);
+
     return (
       <div className="data-entry">
         <div className="data-entry__row">
@@ -47,14 +49,14 @@ class DataEntry extends Component {
           <input
             id="grams_sugar"
             type="number"
-            className="data-entry__input"
+            className={errors.grams ? "data-entry__input error" : "data-entry__input"}
             value={this.state.grams}
             onChange={this.updateGrams.bind(this)}
           />
           <span className="data-entry__units">grams</span>
         </div>
         <div className="data-entry__actions">
-          <button className="data-entry__btn data-entry__btn__primary data-entry__spacer" onClick={this.onSubmit.bind(this)}>Show me</button>
+          <button disabled={isDisabled} className="data-entry__btn data-entry__btn__primary data-entry__spacer" onClick={this.onSubmit.bind(this)}>Show me</button>
           <button className="data-entry__btn" onClick={this.onReset.bind(this)}>Reset</button>
         </div>
       </div>
